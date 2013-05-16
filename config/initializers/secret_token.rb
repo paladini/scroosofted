741cdc3ad499f86cc22f2360f9f4f98b0b04dc64
Fernando Paladini <fernandopalad@gmail.com>
2013-05-16 14:48:37 -0300

    Initial commit
    Signed-off-by: Fernando Paladini <fernandopalad@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scroosofted::Application.config.secret_token = '7ae83d2eacb622b306397e930bde96118b9d5d0d554fb14db9b7c0ec3fb54e1c17bcb1d7771fabe60932722eddc880c60efbe8cae78d8b55ddf776ab6bd98e08'
