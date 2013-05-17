721d5f26b2f0720e3b853e86531ebb8e95e44f4d
Fernando Paladini <fernandopalad@gmail.com>
2013-05-16 21:30:59 -0300

    New visual and improvements.
    Signed-off-by: Fernando Paladini <fernandopalad@gmail.com>

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
 gem "bootstrap-sass", "~> 2.3.1.0"
-gem "pg", "~> 0.15.1"
+gem "sqlite3", "~> 1.3.7"
+gem 'thin'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,28 @@
+class StaticPagesController < ApplicationController
+
+	# Controller for "Home" page
+	def home
+
+	end
+
+	# Controller for "Get The Facts" page
+	def facts
+
+	end
+
+	# Controller for "How They Do It" page
+	def how
+
+	end
+
+	# Controller for "Our Position" page
+	def position
+
+	end
+
+	# Controller for "About" page
+	def about
+
+	end
+
+end
